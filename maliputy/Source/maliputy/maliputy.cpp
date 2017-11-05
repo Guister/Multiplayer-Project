2931f5de8154064a4f637756180bdc16227b1bd3
OpenGameHood <robin.v.grinsven@gmail.com>
2017-11-05 12:52:58 +0000

    comminucation from item to machine and evaluation.
    made the int value pass from item on collision to  player and from player to machine slot. the machine slot yet need to pass values to the main machine . but when that works the evaluation  is already in place.

---
+++ maliputy/Source/maliputy/maliputy.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "maliputy.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, maliputy, "maliputy" );
