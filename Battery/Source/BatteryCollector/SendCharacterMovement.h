dbecfd351187678a25a1fcbcdd6c6f517a3aa294
Guister <guilarga@hotmail.com>
2017-12-14 20:02:10 +0000

    Thursday class

---
+++ Battery/Source/BatteryCollector/SendCharacterMovement.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UDPCommunication.h"
+#include "SendCharacterMovement.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTERYCOLLECTOR_API USendCharacterMovement : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+		FUDPCommunication object;
+	FString message;
+	enum cases {
+		MoveForward,
+		MoveRight
+	};
+
+
+	UFUNCTION(BlueprintCallable, Category = "Communication")
+		FString Comm(int32 id, enum cases, FString data);
+	
+};
