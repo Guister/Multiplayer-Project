c79c16befd7ef9c1d1c29ed2f98fce079b29ccfa
OpenGameHood <robin.v.grinsven@gmail.com>
2017-12-15 12:32:46 +0000

    empty level for lobby/start added client/rps final style

--- Battery/Source/BatteryCollector/RPS.h
+++ Battery/Source/BatteryCollector/RPS.h
@@ -6,15 +6,13 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "RPS.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class BATTERYCOLLECTOR_API URPS : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
-	
-	
-	
+
+	UFUNCTION(BlueprintCallable, Category = "GameMech")
+		static bool RPSWin(int32 OtherTeam, int32 YourTeam, int32 Range);
+	UFUNCTION(BlueprintCallable, Category = "GameMech")
+		static bool RPSWin(int32 OtherTeam, int32 YourTeam, int32 Range);
 };
