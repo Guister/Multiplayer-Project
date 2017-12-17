4c659f23170b8847d0aeb07e529b5c8f14d54667
Guister <guilarga@hotmail.com>
2017-12-17 15:05:40 +0000

    Struggling

--- Battery/Source/BatteryCollector/RPS.cpp
+++ Battery/Source/BatteryCollector/RPS.cpp
@@ -1,16 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-/*#include "RPS.h"
+#include "RPS.h"
 
 
 
 
-bool RPSWin(int OtherTeam, int YourTeam, int Range)
+bool URPS::RPSWin(int32 OtherTeam, int32 YourTeam, int32 Range)
 {
-	int Middle = Range >> 1;//find the middle
+	int32 Middle = Range >> 1;//find the middle
 	YourTeam %= Range;//put your data in range
 	OtherTeam %= Range;//put other team data in range
-	int swift = Middle - OtherTeam;//find the velocity 
-	Yourteam = (Yourteam + swift)%Range;
-	return(YourTeam<Middle)
+	int32 swift = Middle - OtherTeam;//find the velocity 
+	YourTeam = (YourTeam + swift)%Range;
+	return(YourTeam < Middle);
+}
+
+
+/*
+
+M[0:2];
+bool message(Fstring data, enum blabla) {
+	char message;
+	switch blabla
+		case moveforward:
+			message += 'MFA';
+			break;
+
+
+	message += data
 }*/
