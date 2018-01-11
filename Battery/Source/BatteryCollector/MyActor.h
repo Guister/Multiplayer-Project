66f26aa8d9ad298870dc61df37b8ab4e687982c5
Guister <guilarga@hotmail.com>
2018-01-11 15:44:27 +0000

    Revert "Merge branch 'master' of https://github.com/Guister/Multiplayer-Project"
    This reverts commit 931756aeff1841c2bae1044d7ee033b935d43da5, reversing
    changes made to ce7a50ee7f979e98b727f972a473ebc50c3f8da9.

---
+++ Battery/Source/BatteryCollector/MyActor.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "TP_TopDown/TP_TopDownCharacter.h"
+#include "MyActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTERYCOLLECTOR_API AMyActor : public ATP_TopDownCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
